facafb1672f33ebea9f79d9f9976e74a76ea891a
salahcst344 <salahcst344@gmail.com>
2022-11-19 23:45:22 +0200

    Add routes

--- shareme_frontend/src/App.js
+++ shareme_frontend/src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Login from "./components/Login";
+import Home from "./container/Home";
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  return (
+    <GoogleOAuthProvider clientId="890185293385-c206mg9ecn3fl0qglndrli176o593o3m.apps.googleusercontent.com">
+      <Routes>
+        <Route path="login" element={<Login />} />
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </GoogleOAuthProvider>
+  );
 };
 
 export default App;

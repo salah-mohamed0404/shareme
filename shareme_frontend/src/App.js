dbd51abe57d3708e2554293643e9ffdee6d94d4e
salahcst344 <salahcst344@gmail.com>
2022-11-24 15:42:47 +0200

    Force to login

--- shareme_frontend/src/App.js
+++ shareme_frontend/src/App.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import { Login } from "./components";
 import Home from "./container/Home";
+import { fetchUser } from "./utils/fetchUser";
 
 const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = fetchUser();
+
+    if (!user) navigate("/login");
+  }, []);
+
   return (
     <GoogleOAuthProvider clientId="890185293385-c206mg9ecn3fl0qglndrli176o593o3m.apps.googleusercontent.com">
       <Routes>

c79037cc17cb790bb5f8f19e6b91795c557905be
salahcst344 <salahcst344@gmail.com>
2022-11-24 15:03:57 +0200

    Build Search component

--- shareme_frontend/src/components/Search.jsx
+++ shareme_frontend/src/components/Search.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function Search() {
-  return <div>Search</div>;
+import MasonryLayout from "./MasonryLayout";
+import { client } from "../client";
+import { feedQuery, searchQuery } from "../utils/data";
+import Spinner from "./Spinner";
+
+function Search({ searchTerm }) {
+  const [pins, setPins] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (searchTerm) {
+      setLoading(true);
+      const query = searchQuery(searchTerm.toLowerCase());
+
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [searchTerm]);
+
+  return (
+    <div>
+      {loading ? <Spinner message="Searching for pins..." /> : null}
+      {pins?.length !== 0 && <MasonryLayout pins={pins} />}
+      {pins?.length === 0 && searchTerm !== "" && !loading && (
+        <div className="mt-10 text-center text-xl">
+          Unfortunately no pins found!
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Search;
